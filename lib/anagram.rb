3a3d5d4ee3237d0d4f5e916007215ceb726550c8
Eva <yi.zheng1709@gmail.com>
2021-01-18 13:59:32 -0500

    commit tree 0985882cc82b31708f4ab971e2f46e9a85527a2e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word) 
+        @word = word   
+    end 
+
+
+    def match(list)
+        array = []
+        list.each do |possible_anagram|
+            if @word.split("").sort == possible_anagram.split("").sort 
+                array << possible_anagram
+            end 
+        end 
+        array 
+    end 
+end 
